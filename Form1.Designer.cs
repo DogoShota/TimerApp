707ce83bf450a916a3259d73323e3a557f7f8600
DogoShota <shota.dogo0084@gmail.com>
2024-06-23 18:32:10 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TimerApp2
+{
+    partial class KitchenTimer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KitchenTimer));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.AddMinute = new System.Windows.Forms.Button();
+            this.AddSec = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.AddHour = new System.Windows.Forms.Button();
+            this.TimerLabel = new System.Windows.Forms.TextBox();
+            this.SetTimer = new System.Windows.Forms.Timer(this.components);
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("メイリオ", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StartButton.Location = new System.Drawing.Point(244, 392);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(185, 95);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // AddMinute
+            // 
+            this.AddMinute.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.AddMinute.Location = new System.Drawing.Point(469, 318);
+            this.AddMinute.Name = "AddMinute";
+            this.AddMinute.Size = new System.Drawing.Size(185, 52);
+            this.AddMinute.TabIndex = 1;
+            this.AddMinute.Text = "1分追加";
+            this.AddMinute.UseVisualStyleBackColor = true;
+            this.AddMinute.Click += new System.EventHandler(this.AddMinute_Click);
+            // 
+            // AddSec
+            // 
+            this.AddSec.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.AddSec.Location = new System.Drawing.Point(692, 318);
+            this.AddSec.Name = "AddSec";
+            this.AddSec.Size = new System.Drawing.Size(185, 52);
+            this.AddSec.TabIndex = 2;
+            this.AddSec.Text = "10秒追加";
+            this.AddSec.UseVisualStyleBackColor = true;
+            this.AddSec.Click += new System.EventHandler(this.AddSec_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("メイリオ", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StopButton.ForeColor = System.Drawing.Color.Blue;
+            this.StopButton.Location = new System.Drawing.Point(469, 392);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(185, 95);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // AddHour
+            // 
+            this.AddHour.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.AddHour.Location = new System.Drawing.Point(244, 318);
+            this.AddHour.Name = "AddHour";
+            this.AddHour.Size = new System.Drawing.Size(185, 52);
+            this.AddHour.TabIndex = 4;
+            this.AddHour.Text = "1時間追加";
+            this.AddHour.UseVisualStyleBackColor = true;
+            this.AddHour.Click += new System.EventHandler(this.AddHour_Click);
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.TimerLabel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TimerLabel.Font = new System.Drawing.Font("Times New Roman", 100.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerLabel.Location = new System.Drawing.Point(66, 41);
+            this.TimerLabel.Multiline = true;
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TimerLabel.Size = new System.Drawing.Size(800, 250);
+            this.TimerLabel.TabIndex = 5;
+            this.TimerLabel.Text = "00:00:00";
+            this.TimerLabel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SetTimer
+            // 
+            this.SetTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Font = new System.Drawing.Font("メイリオ", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ResetButton.ForeColor = System.Drawing.Color.Red;
+            this.ResetButton.Location = new System.Drawing.Point(692, 392);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(185, 95);
+            this.ResetButton.TabIndex = 6;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // KitchenTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 517);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.AddHour);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.AddSec);
+            this.Controls.Add(this.AddMinute);
+            this.Controls.Add(this.StartButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "KitchenTimer";
+            this.Text = "KitchenTimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button AddMinute;
+        private System.Windows.Forms.Button AddSec;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button AddHour;
+        private System.Windows.Forms.TextBox TimerLabel;
+        private System.Windows.Forms.Timer SetTimer;
+        private System.Windows.Forms.Button ResetButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerApp2
+{
+    public partial class KitchenTimer : Form
+    {
+        public KitchenTimer()
+        {
+            InitializeComponent();
+        }
+
+        // 計測時間
+        int sec = 0;
+        private void ViewTime()
+        {
+            TimerLabel.Text = "" + sec / 36000 % 10 + sec / 3600 % 10 +
+                              ":" + sec / 600 % 6 + sec / 60 % 10 +
+                              ":" + sec / 10 % 6 + sec % 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sec--;
+            if (0 == sec)
+            {
+                // タイマー終了時に警告音を鳴らす
+                SetTimer.Enabled = false;
+                System.Media.SystemSounds.Asterisk.Play();
+                this.Activate();
+            }
+            ViewTime();
+        }
+
+        // タイマーに1時間追加する
+        private void AddHour_Click(object sender, EventArgs e)
+        {
+            sec += 3600;
+            if (sec >= 360000) sec = 0;
+            ViewTime();
+        }
+
+        // タイマーに1分追加する
+        private void AddMinute_Click(object sender, EventArgs e)
+        {
+            sec += 60;
+            ViewTime();
+        }
+
+        // タイマーに10秒追加する
+        private void AddSec_Click(object sender, EventArgs e)
+        {
+            sec += 10;
+            ViewTime();
+        }
+
+        // スタートボタンをクリック時
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (0 == sec) return;
+            SetTimer.Enabled = true;
+            this.StopButton.Enabled = true;
+            this.StartButton.Enabled = false;
+            this.AddHour.Enabled = false;
+            this.AddMinute.Enabled = false;
+            this.AddSec.Enabled = false;
+            this.ResetButton.Enabled = false;
+        }
+
+        // ストップボタンをクリック時
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            SetTimer.Enabled = false;
+            this.StopButton.Enabled = false;
+            this.StartButton.Enabled = true;
+            this.AddHour.Enabled = true;
+            this.AddMinute.Enabled = true;
+            this.AddSec.Enabled = true;
+            this.ResetButton.Enabled = true;
+        }
+
+        // リセットボタンをクリック時、カウントを0に戻す
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            TimerLabel.Text = "00:00:00";
+            sec = 0;
+        }
+    }
+}
